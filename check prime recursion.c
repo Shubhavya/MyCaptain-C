d941e76000c758ca902c3d4068e4da46a6c409c2
Shubhavya <64183910+Shubhavya@users.noreply.github.com>
2020-12-31 12:03:49 +0530

    Add files via upload
    C program to check if a number is a prime number or not using recursion.

---
+++ check prime recursion.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int checkPrime(int);
+
+int main()
+{
+	int x, flag;
+	printf("Enter a number to check prime or not: ");
+	scanf("%d", &x);
+	flag = checkPrime(x);
+	if (flag==1)
+		printf("%d is a prime number!", x);
+	else
+		printf("%d is not a prime number!", x);
+	return 0;
+}
+
+int checkPrime(int x)
+{
+	int i=x/2;
+	if (i==1)
+	{
+		return 1;
+	}
+	else if (x%i==0)
+	{
+		return 0;
+	}
+	else
+	{
+		i--;
+		checkPrime(x);
+	}
+	
+}
